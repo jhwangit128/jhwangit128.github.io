372e0b9e3a9419b5b45320ba1f48117bc2c3926a
Jiha Hwang <jiha_hwang@jihas-mbp.lan>
2019-07-21 00:20:51 -0500

    totally forgot to commit fixing css

--- app.js
+++ app.js
@@ -6,53 +6,13 @@ $(() => {
   // hide them all!
   // $('#search').hide()
   $('.wrapper').hide()
+  // $('.carousel-container').hide()
 
-
-  // =====================
-  // MAIN BUTTON CAROUSEL
-  // =====================
-  let currentBtnIndex = 0
-  let $currentBtn = $('.text-carousel').children().eq(currentBtnIndex)
-  let numOfBtn = $('.text-carousel').children().length - 1
-
-  const $next = $('.next')
-  const $previous = $('.previous')
-  //next btn
-  $next.on('click', () => {
-    $currentBtn.hide()
-
-
-    if(currentBtnIndex < numOfBtn) {
-      currentBtnIndex++
-    } else {
-      currentBtnIndex = 0
-    }
-    $currentBtn = $('.text-carousel').children().eq(currentBtnIndex)
-
-    $currentBtn.show()
-  })
-
-  //prev btn
-  $previous.on('click', () => {
-    $currentBtn.hide()
-
-    if(currentBtnIndex > 0) {
-      currentBtnIndex--
-    } else {
-      currnetBtnIndex = numOfBtn
-    }
-
-    $currentBtn = $('.text-carousel').children().eq(currentBtnIndex)
-
-    $currentBtn.show()
-
-  })
-
-  // BUTTONS
-
-  // $('#non-alcoholic').on('click', () => {
-  //   $('#header').hide()
-  // })
+  // BUTTON
+ $('.wrapper-close').on('click', () => {
+   $('.wrapper').hide()
+   $('.carousel-container').show()
+ })
 
   // ================
   // DRINK TRACKER
@@ -61,10 +21,12 @@ $(() => {
   // DRINK TRACKER BUTTON
   $('#tracker-btn').on('click', (event) => {
     $('#tracker-modal').show()
+    $('.carousel-container').hide()
   })
 
   $('#tracker-close').on('click', () => {
     $('#tracker-modal').hide()
+    $('.carousel-container').show()
   })
 
   // BACKGROUND COLOR CHANGES WHEN CERTAIN NUM OF BUTTON CLICKED
@@ -82,100 +44,80 @@ $(() => {
         }
   })
 
+  // ======================
+  // IMAGE CAROUSEL RESULTS
+  // ======================
+  // $('#non-alcoholic').on('click', () => {
+  //   $('.carousel-container').show()
+  // })
+    $.ajax({
+        url:'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
 
-  // ================
-  // IMAGE CAROUSEL
-  // ================
-  // let currentImgIndex = 0
-  //
-  // let $currentImg = $('.carousel-images').children().eq(currentImgIndex)
-  //
-  // let numOfImages = $('.carousel-images').children().length - 1
-  //
-  // const $next = $('.next')
-  // const $previous = $('.previous')
-  //
-  // $next.on('click', () => {
-  //
-  //   $currentImg.hide()
-  //   // check if the currentImgIndex is less than the amount of images we have
-  //   if(currentImgIndex < numOfImages) {
-  //     // increment current image index
-  //     currentImgIndex++
-  //   } else { // if the currentImgIndex > the amount of images we have
-  //     // reset the currentImgIndex to 0, so we cycle back
-  //     currentImgIndex = 0
-  //   }
-  //   // change the currentImg
-  //   $currentImg = $('.carousel-images').children().eq(currentImgIndex)
-  //   // show the new currentImg
-  //   $currentImg.show()
+      }).then(
+      (data) => {
+
+        const $random = data.drinks
+        for(let j = 0; j < $random.length; j++){
+        const $imagesRan = $random[j].strDrinkThumb
+        const $imgThumbRan = $('<img>').attr("src" , $imagesRan).attr('id', 'imgRan' + j).addClass('imageRan')
+        $('#img-carousel').append($imgThumbRan)
+        console.log($imgThumbRan)
+
+        // ================
+        // IMAGE CAROUSEL
+        // ================
+        let currentImgIndex = 0
+
+        let $currentImg = $('#img-carousel').children('img').eq(0)
+
+        let numOfImages = 50
+        // console.log($currentImg)
+        const $next = $('.next')
+        const $previous = $('.previous')
+
+        $next.on('click', () => {
+          $currentImg.hide()
+
+          if(currentImgIndex < 50) {
+            currentImgIndex++
+          } else {
+            currentImgIndex = 0
+          }
+
+          $currentImg = $('#img-carousel').children().eq(currentImgIndex)
+          $currentImg.show()
+        })
+
+        $previous.on('click', () => {
+          $currentImg.hide()
+
+          if (currentImgIndex > 0) {
+            currentImgIndex--
+          } else {
+            currentImgIndex = 50
+          }
+
+          $currentImg = $('#img-carousel').children().eq(currentImgIndex)
+          $currentImg.show()
+        })
+
+      }
+
+      },
+      (error) => {
+      console.log(error)
+
+    })
   // })
-  //
-  // // previous button
-  // $previous.on('click', () => {
-  //   // hide the current image
-  //   $currentImg.hide()
-  //   // check if the currentImgIndex > 0
-  //   if (currentImgIndex > 0) {
-  //     // decrement the current image index
-  //     currentImgIndex--
-  //   } else { // if the currentImgIndex < 0, reset the currentImgIndex to the numOfImages
-  //     currentImgIndex = numOfImages
-  //   }
-  //   // change the currentImg
-  //   $currentImg = $('.carousel-images').children().eq(currentImgIndex)
-  //   // show the new currentImg
-  //   $currentImg.show()
-  // })
-  // ================
-  // SEARCH BAR
-  // ================
-  // $('#non-alcoholic').on('click', (event) => {
-  //   // event.preventDefault()
-  //   // $('#name').empty();
-  //   // const userInput = $('input[type="text"]').val()
-  //
-  //   $.ajax({
-  //       url:'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic',
-  //       type: "GET",
-  //       dataType: 'json',
-  //     }).then(
-  //     (data) => {
-  //
-  //       const $dataNonAlco = data.drinks
-  //       const $imgUlNa = $('<ul>')
-  //       const $imgLiNa= $('<li>')
-  //       for(let j = 0; j < $dataNonAlco.length; j++){
-  //       const $imagesNa = $dataNonAlco[j].strDrinkThumb
-  //       const $imgThumbNa = $('<img>').attr("src" , $imagesNa).attr('id', 'imgNa' + j).addClass('imageNa')
-  //       $imgLiNa.append($imgThumbNa)
-  //       $imgUlNa.append($imgLiNa)
-  //       $('.search-images').append($imgUlNa)
-  //       console.log($dataNonAlco)
-  //       //   $nameBtn.on('click', () => {
-  //       //     $recipeLi.toggle()
-  //       //     $ingreBtn.toggle()
-  //       //     $imgThumb.toggle()
-  //       //   })
-  //       // $recipeLi.css('display','none')
-  //       // $ingreBtn.css('display', 'none')
-  //       // $imgThumb.css('display','none')
-  //
-  //     }
-  //
-  //     },
-  //     (error) => {
-  //     console.log(error)
-  //
-  //   })
-  // })
+
+
 
   $('#main-btn').on('click', (event) => {
     event.preventDefault()
     $('#name').empty()
-    $('.header').hide()
     $('.wrapper').show()
+    $('.carousel-container').hide()
+
     const userInput = $('input[type="text"]').val()
 
     $.ajax({
@@ -201,12 +143,8 @@ $(() => {
         const $ingredient4 = $data[i].strIngredient4
         const $ingredient5 = $data[i].strIngredient5
         const $ingredients = [$ingredient1, $ingredient2, $ingredient3, $ingredient4, $ingredient5]
-
-
-        console.log($data)
-
         const $images = $data[i].strDrinkThumb
-        // const $li = $('<li>').text($name)
+        // console.log($data)
         const $nameBtn = $('<button>').text($name).attr('id', 'name' + i).addClass('nameBtn')
         // const $liName = $li.attr('id','name' + i)
         $li.append($nameBtn)
@@ -227,7 +165,6 @@ $(() => {
         $ingredientUl.append($ingredientLi)
         $('#ingredients').append($ingredientUl)
 
-        console.log($ingredients)
           //buttons for each drinks
           $nameBtn.on('click', () => {
             $recipeLi.toggle()
